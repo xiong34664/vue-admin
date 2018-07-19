a95e9d0cb48f3278197b71d5f4208a4364d6ac9e
不忘初心 <xjh34664@163.com>
2018-07-19 22:21:38 +0800

    设置路由全局守卫

--- src/main.js
+++ src/main.js
@@ -13,10 +13,29 @@ import '@/style/index.scss'
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
+// 注册全局守卫 在路由跳转前 对路由进行跳转 防止未登录的用户跳转到其他页面 还将已登录的用户  访问 /login 跳转至home
+router.beforeEach((to, from, next) => {
+  let token = localStorage.xxtoken
+  if (token) {
+    // 登陆了就放行
+    next()
+    if (to.path === '/login') {
+      next({ path: '/home' })
+    }
+  } else if (to.path !== '/login') {
+    next({ path: '/login' })
+  } else {
+    // 如果没有登录访问的又是login  就不干涉
+    next()
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })

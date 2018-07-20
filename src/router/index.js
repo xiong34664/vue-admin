17306f6c5dd742f5d0ad0595c46441a098655874
不忘初心 <xjh34664@163.com>
2018-07-20 10:21:03 +0800

    头部部分功能及中间内容跳转

--- src/router/index.js
+++ src/router/index.js
@@ -2,25 +2,25 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/views/Login'
 import Home from '@/views/Home'
+import Main from '@/views/main/Main.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'Default',
-      redirect: {name: 'Login'}
-    },
-    {
       path: '/login',
       name: 'Login',
       component: Login
     },
     {
-      path: '/home',
+      path: '/',
       name: 'Home',
-      component: Home
+      component: Home,
+      children: [
+        {name: 'default', path: '/', redirect: {name: 'main'}},
+        {name: 'main', path: 'main', component: Main}
+      ]
     }
   ]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,46 +1,27 @@
 <template>
     <div class="home">
         <el-container>
-
-            <el-aside width="200px">
-                <el-radio-group v-model="isCollapse"
-                                style="margin-bottom: 20px;">
-                    <el-radio-button :label="false">展开</el-radio-button>
-                    <el-radio-button :label="true">收起</el-radio-button>
-                </el-radio-group>
-                <el-menu default-active="2"
-                         class="el-menu-vertical-demo"
-                         @open="handleOpen"
-                         @close="handleClose"
-                         :collapse="isCollapse">
+            <el-aside width="auto">
+                <div class="logo"></div>
+                <el-menu default-active="1-1"
+                         class="el-menu-admin"
+                         :collapse='isCollapse'
+                         :router='true'>
                     <el-submenu index="1">
                         <template slot="title">
                             <i class="el-icon-location"></i>
-                            <span>导航一</span>
+                            <span>用户管理</span>
                         </template>
                         <el-menu-item-group>
-                            <template slot="title">分组一</template>
-                            <el-menu-item index="1-1">选项1</el-menu-item>
-                            <el-menu-item index="1-2">选项2</el-menu-item>
+                            <el-menu-item index="1-1">
+                                <i class="el-icon-location"></i>用户列表</el-menu-item>
                         </el-menu-item-group>
-                        <el-menu-item-group title="分组2">
-                            <el-menu-item index="1-3">选项3</el-menu-item>
-                        </el-menu-item-group>
-                        <el-submenu index="1-4">
-                            <template slot="title">选项4</template>
-                            <el-menu-item index="1-4-1">选项1</el-menu-item>
-                        </el-submenu>
                     </el-submenu>
                     <el-menu-item index="2">
                         <i class="el-icon-menu"></i>
                         <span slot="title">导航二</span>
                     </el-menu-item>
-                    <el-menu-item index="3"
-                                  disabled>
-                        <i class="el-icon-document"></i>
-                        <span slot="title">导航三</span>
-                    </el-menu-item>
-                    <el-menu-item index="4">
+                    <el-menu-item index="3">
                         <i class="el-icon-setting"></i>
                         <span slot="title">导航四</span>
                     </el-menu-item>
@@ -49,10 +30,11 @@
 
             <el-container>
                 <el-header>
+                    <i class="xxicon xxicon-menu toggle-btn"
+                       @click="toggleCollapse"></i>
                     <el-menu :default-active="activeIndex"
                              class="el-menu-demo"
-                             mode="horizontal"
-                             @select="handleSelect">
+                             mode="horizontal">
                         <el-menu-item index="1">处理中心</el-menu-item>
                         <el-submenu index="2">
                             <template slot="title">我的工作台</template>
@@ -73,22 +55,12 @@
                                target="_blank">订单管理</a>
                         </el-menu-item>
                     </el-menu>
+                    <el-button type="text"
+                               @click="logout">退出登录</el-button>
+
                 </el-header>
                 <el-main>
-                    <el-table :data="userList.users"
-                              style="width: 100%">
-                        <el-table-column prop="role_name"
-                                         label="日期"
-                                         width="180">
-                        </el-table-column>
-                        <el-table-column prop="username"
-                                         label="姓名"
-                                         width="180">
-                        </el-table-column>
-                        <el-table-column prop="mobile"
-                                         label="地址">
-                        </el-table-column>
-                    </el-table>
+                    <router-view/>
                 </el-main>
             </el-container>
         </el-container>
@@ -113,17 +85,62 @@ export default {
     })
   },
   methods: {
-    handleSelect (key, keyPath) {
-      console.log(key, keyPath)
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
     },
-    handleOpen (key, keyPath) {
-      console.log(key, keyPath)
-    },
-    handleClose (key, keyPath) {
-      console.log(key, keyPath)
+    logout () {
+      // 清除登入状态即保存在 localStorage中的token
+      localStorage.removeItem('xxtoken')
+      // 跳转到登录页面
+      this.$router.push({ name: 'Login' })
     }
   }
 }
 </script>
 <style lang="scss" scoped>
+$color: #409eff;
+
+.home {
+  height: 100%;
+  background-color: #e5e5e5;
+  .el-menu-admin:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .el-container {
+    height: 100%;
+  }
+  .el-aside {
+    background-color: #fff;
+  }
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #fff;
+  }
+  .logo {
+    height: 60px;
+    background: #fff url(../assets/logo.png) no-repeat center / contain;
+    // background-size: cover;
+  }
+  .toggle-btn {
+    padding: 0 10px;
+    margin-left: -20px;
+    font-size: 36px;
+    line-height: 60px;
+    color: #35495e;
+    cursor: pointer;
+    &:hover {
+      background-color: #ecf5ff;
+    }
+  }
+  .system-title {
+    font-size: 28px;
+    color: white;
+  }
+  .welcome {
+    color: white;
+  }
+}
 </style>
